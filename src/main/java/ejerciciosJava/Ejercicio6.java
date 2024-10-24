e350f0b3c57709f7484b15150c3201ded2c3fae7
alcinacarlos <calcrom0607@g.educaand.es>
2024-10-24 16:32:20 +0200

    cooking

---
+++ src/main/java/ejerciciosJava/Ejercicio6.java
@@ -0,0 +1,11 @@
+package ejerciciosJava;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 1; i <= 100; i++) {
+            num += i;
+        }
+        System.out.println(num);
+    }
+}
